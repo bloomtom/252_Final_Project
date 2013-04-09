2de60999d813346b97a499f4b7e0856e0b35ab9d
Thomas Bloom <bloomtom@gmail.com>
2013-04-09 05:50:15 -0400

    Added ServerAPI
    Added the API clients can use to manage a connection to a TheNoise
    server.
    This API is a mockup at this point, it is only a base model for
    implementation.

---
+++ TheNoise Server/TheNoise ServerAPI/ServerConnection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using TcpTransmission;
+using TcpTransmission.Client;
+
+namespace TheNoiseAPI
+{
+    public class ServerConnection : IDisposable
+    {
+        private ClientConnection client;
+
+        public ServerConnection(IPAddress ip, int port)
+        {
+            client = new ClientConnection(ip, port);
+        }
+
+        public int Register()
+        {
+            return 0;
+        }
+
+        public int Authenticate()
+        {
+            return 0;
+        }
+
+        public void RequestAudioList()
+        {
+            return;
+        }
+
+        public void RequestBeginAudioStream(string trackName)
+        {
+            return;
+        }
+
+        public void RequestEndAudioStream()
+        {
+            return;
+        }
+
+        public void OpenConnection()
+        {
+            client.Connect();
+        }
+
+        public void CloseConnection()
+        {
+            client.Close();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Close all managed resources.
+                CloseConnection();
+            }
+
+            // Dispose of unmanaged resources here if any.
+        }
+    }
+}
