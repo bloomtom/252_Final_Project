0e3ef80756d1ea2661bb3020cece0c41b97d2ca7
Matthew Lindsay <thespian42@gmail.com>
2013-04-22 12:09:04 -0400

    Added New Database Testing Project
    As in title.

---
+++ Database Testing Project/Example_9_StoredProcParams/Program.cs
@@ -0,0 +1,88 @@
+﻿/**
+ * Example_8
+ * Stored procedures and functions with parameters
+ * Requires departments database. 
+ **/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Data.SqlClient; // for SqlDataAdapter
+using System.Data;           // for DataSet
+
+namespace Example_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string connectionString = "Integrated Security=true;" +
+                                "database=userFiles;" +
+                                "Data Source=.;"; // local machine
+
+            SqlConnectionStringBuilder sqlStrBldr = new SqlConnectionStringBuilder(connectionString);
+            sqlStrBldr.UserID = "sa";
+            sqlStrBldr.Password = "nhti";
+
+            SqlConnection connection = new SqlConnection(sqlStrBldr.ConnectionString); //  new SqlConnection(connectionString);
+
+            SqlCommand testCommand = new SqlCommand("INSERT VALUES 'Test4', 'Test4PW' INTO musicUsers");
+            testCommand.ExecuteNonQuery();
+
+            Console.WriteLine(testCommand.ToString());
+
+
+            SqlCommand command = new SqlCommand("checkMusicUser", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter userName = new SqlParameter("@uname", SqlDbType.VarChar);
+            userName.Direction = ParameterDirection.Input;
+            userName.Value = "Test1";
+            userName.Size = 50;
+            command.Parameters.Add(userName);
+
+            SqlParameter password = new SqlParameter("@passw", SqlDbType.VarChar);
+            password.Direction = ParameterDirection.Input;
+            password.Value = "Test1PW";
+            password.Size = 50;
+            command.Parameters.Add(password);
+
+            SqlParameter returnValue = new SqlParameter("@retval", SqlDbType.Int);
+            returnValue.Direction = ParameterDirection.Output;
+            command.Parameters.Add(returnValue);
+
+            
+
+            command.Connection.Open();
+
+            command.ExecuteNonQuery();
+
+            Console.WriteLine("TEST");
+            Console.WriteLine("Return result: " + command.Parameters["@retval"].Value.ToString());
+        }
+
+        private bool executeNonQuery(string sql, SqlConnectionStringBuilder sqlStrBldr)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(sqlStrBldr.ConnectionString))
+                {
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.Connection.Open();
+                    if (command.ExecuteNonQuery() < 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Database Testing Project/Example_9_StoredProcParams/_vti_cnf/Program.cs
@@ -0,0 +1,5 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|31 Oct 2010 21:50:31 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_cacheddtm:TX|31 Oct 2010 21:50:31 -0000
+vti_filesize:IR|2967
