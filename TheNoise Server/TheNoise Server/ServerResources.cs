afdac7a2b53eaf62a6b70c5baa3a31fbecb601d5
Thomas Bloom <bloomtom@gmail.com>
2013-05-05 06:46:30 -0400

    Partitioned Server Manager
    The server management system is now spread across three code files to
    improve readability.

---
+++ TheNoise Server/TheNoise Server/ServerResources.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheNoiseHLC;
+using TheNoiseHLC.CommunicationObjects;
+using TheNoiseHLC.CommunicationObjects.AudioTrack;
+using TheNoiseHLC.CommunicationObjects.GlobalEnumerations;
+using TcpTransmission;
+using TcpTransmission.Client;
+using System.Net;
+
+namespace TheNoise_Server
+{
+    /// <summary>
+    /// Contains event data for large packets of audio.
+    /// </summary>
+    internal sealed class AudioPacketEventArgs : EventArgs
+    {
+        public byte[] data;
+
+        public AudioPacketEventArgs(byte[] data)
+        {
+            this.data = data;
+        }
+        public AudioPacketEventArgs()
+        {
+        }
+    }
+
+    /// <summary>
+    /// Provides a method of delivering event data regarding client connect or disconnect.
+    /// </summary>
+    public sealed class ClientAuthEventArgs : EventArgs
+    {
+        private IPEndPoint clientIPE;
+        public IPEndPoint ClientIPE
+        {
+            get { return clientIPE; }
+            set { }
+        }
+
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set { }
+        }
+
+        public ClientAuthEventArgs(IPEndPoint clientIPE, string clientUsername)
+        {
+            this.clientIPE = clientIPE;
+            this.username = clientUsername;
+        }
+    }
+
+    /// <summary>
+    /// Class for general info events regarding internal server activity.
+    /// </summary>
+    public sealed class GeneralEventArgs : EventArgs
+    {
+        public IPEndPoint SubjectIPE { get; private set; }
+        public string SubjectUsername { get; private set; }
+        public string EventMessage { get; private set; }
+
+        public GeneralEventArgs(string subjectUsername, IPEndPoint subjectIPE, string eventMessage)
+        {
+            SubjectIPE = subjectIPE;
+            SubjectUsername = subjectUsername;
+            EventMessage = eventMessage;
+        }
+
+        public override string ToString()
+        {
+            if (EventMessage == null)
+            {
+                return base.ToString();
+            }
+
+            if (SubjectUsername == null && SubjectIPE == null)
+            {
+                return "General Event: " + EventMessage;
+            }
+            else if (SubjectUsername != null && SubjectIPE != null)
+            {
+                return String.Format("Client {0} at {1} {2}", SubjectUsername, SubjectIPE, EventMessage);
+            }
+            else if (SubjectUsername != null)
+            {
+                return String.Format("{0}: {1}", SubjectUsername, EventMessage);
+            }
+            else if (SubjectIPE != null)
+            {
+                return String.Format("Client at {0} {1}", SubjectIPE, EventMessage);
+            }
+
+
+            return base.ToString(); // Why would this ever be hit?
+        }
+    }
+}
---
+++ TheNoise Server/TheNoise Server/TheNoiseClientHandler.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheNoiseHLC;
+using TheNoiseHLC.CommunicationObjects;
+using TheNoiseHLC.CommunicationObjects.AudioTrack;
+using TheNoiseHLC.CommunicationObjects.GlobalEnumerations;
+using TcpTransmission;
+using TcpTransmission.Client;
+using System.Net;
+
+namespace TheNoise_Server
+{
+
+    /// <summary>
+    /// Handles TheNoise server specific client serving capability.
+    /// </summary>
+    internal sealed class TheNoiseClientHandler
+    {
+        public delegate void AudioPacketEventHandler(IPEndPoint sender, AudioPacketEventArgs e);
+        public event AudioPacketEventHandler audioPacketReady = delegate { };
+
+        public delegate void TrackListUpdatedEventHandler(IPEndPoint sender, TrackList e);
+        public event TrackListUpdatedEventHandler trackListUpdated = delegate { };
+
+        public delegate void ClientSendBackHandler(IPEndPoint sender, IncomingMessageEventArgs e);
+        public event ClientSendBackHandler ClientSendBack = delegate { };
+
+        private string username; // The username this client authenticated with.
+        public string Username
+        {
+            get { return username; }
+            set { }
+        }
+
+        private string audioPath;
+        public string AudioPath
+        {
+            get { return audioPath; }
+            set { }
+        }
+
+        private IPEndPoint tcpEndPoint;
+        private IPEndPoint udpEndPoint;
+
+        private AwesomeAudio.UDPSender audioSender;
+
+        private long lastUpdatedMusicList;
+
+        public TheNoiseClientHandler(string username, string audioPath, IPEndPoint ipe)
+        {
+            this.username = username;
+            this.audioPath = audioPath;
+            tcpEndPoint = ipe;
+
+            if (!System.IO.Directory.Exists(audioPath))
+            {
+                System.IO.Directory.CreateDirectory(audioPath);
+            }
+        }
+
+
+        /// <summary>
+        /// Deprecated. Use BeginStreaming only.
+        /// </summary>
+        public void ContinueStream()
+        {
+
+        }
+
+        /// <summary>
+        /// Begins the streaming.
+        /// </summary>
+        /// <param name="audioTrack">The audio track.</param>
+        public void BeginStreaming(TrackStreamRequest audioTrack)
+        {
+            this.udpEndPoint = audioTrack.Connection;
+            string trackLocation = audioPath + audioTrack.Track.TrackName + audioTrack.Track.TrackExtension;
+
+            audioSender = new AwesomeAudio.UDPSender(udpEndPoint.Address.ToString(), trackLocation);
+            audioSender.StartFileSend();
+        }
+
+        public void EndStreaming()
+        {
+            if (audioSender != null)
+            {
+                audioSender.StopFileSend();
+            }
+
+            audioSender = null;
+        }
+
+        public void RequestUpdateList()
+        {
+            // Hack to prevent firing of this event too quickly.
+            if (DateTime.UtcNow.Ticks > (lastUpdatedMusicList + 50000000)) // 5 seconds
+            {
+                lastUpdatedMusicList = DateTime.UtcNow.Ticks;
+
+                // Get every file in the user's directory and list them.
+                AwesomeAudio.WaveFileManager audioInfo = new AwesomeAudio.WaveFileManager();
+                string[] files = System.IO.Directory.GetFiles(audioPath);
+                Track[] tracks = new Track[files.Length];
+                for (int i = 0; i < files.Length; i++)
+                {
+                    audioInfo.ReadWaveFile(files[i]);
+                    int secondsLong = audioInfo.NumTenthOfSeconds / 10;
+                    string trackName = System.IO.Path.GetFileNameWithoutExtension(files[i]);
+                    string trackExt = System.IO.Path.GetExtension(files[i]);
+                    tracks[i] = new Track(trackName, trackExt, secondsLong, TrackType.Unspecified);
+                }
+
+                trackListUpdated.Invoke(tcpEndPoint, new TrackList(tracks));
+            }
+        }
+    }
+}
